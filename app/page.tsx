abe80380a6d10e3a4faa2037ae3ff8e162c30d64
yanwlGit <yanwenlong_z@163.com>
2024-04-22 23:55:36 +0800

    update image width

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
             <Image
               src="/hero-desktop.png"
-              width={1000}
+              width={800}
               height={760}
               className="hidden md:block"
               alt="Screenshots of the dashboard project showing desktop version"
