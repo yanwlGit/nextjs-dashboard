e709327a58efb284a10aff0b827152081da9ad71
yanwlGit <yanwenlong_z@163.com>
2024-04-23 00:44:29 +0800

    resolve image dont show

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,9 @@
-import { Inter } from 'next/font/google';
+import { Inter, Lusitana } from 'next/font/google';
 
- 
+
 export const inter = Inter({ subsets: ['latin'] });
 
-export const lusitana = Inter({ subsets: ['latin'],weight:"100" });
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
